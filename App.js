69fc66f34530d3afe550e4e3811e8634463f6907
ssam1213 <wonbok1213@gmail.com>
2020-10-17 14:21:50 +0900

    commit test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to starts working on your app!</Text>
+      <Text>Open up App.js to asdstartsgit working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
