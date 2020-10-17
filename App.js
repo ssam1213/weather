6a6c783dff6efcb5d0b59c006387fd9aa199d2e4
ssam1213 <wonbok1213@gmail.com>
2020-10-17 14:32:05 +0900

    asd

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to asdstartsgit wasdorking on your app!</Text>
+      <Text>Open up App.js to asdstartsgit wasasddorking on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
